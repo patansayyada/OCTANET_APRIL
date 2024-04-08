86529c77ec08565965e62c21b0904d07196cd6a4
patansayyada <144011918+patansayyada@users.noreply.github.com>
2024-04-08 14:19:09 +0530

    Add files via upload

--- script.js
+++ script.js
@@ -1,8 +1,36 @@
-function showTab(tabName) {
-    var i, tabContent;
-    tabContent = document.getElementsByClassName("tab-content");
-    for (i = 0; i < tabContent.length; i++) {
-        tabContent[i].style.display = "none";
-    }
-    document.getElementById(tabName).style.display = "block";
+// JavaScript to toggle visibility of product sections
+function showHome() {
+  hideAllSections();
 }
+
+function showMenProducts() {
+  hideAllSections();
+  document.getElementById('men').style.display = 'block';
+}
+
+function showWomenProducts() {
+  hideAllSections();
+  document.getElementById('women').style.display = 'block';
+}
+
+function showKidsProducts() {
+  hideAllSections();
+  document.getElementById('kids').style.display = 'block';
+}
+
+function showAbout() {
+  hideAllSections();
+  document.getElementById('about').style.display = 'block';
+}
+
+function hideAllSections() {
+  const sections = document.querySelectorAll('.products, .about-section');
+  sections.forEach(section => {
+    section.style.display = 'none';
+  });
+}
+
+// Event listeners to show products when options are clicked
+document.getElementById('men').addEventListener('click', showMenProducts);
+document.getElementById('women').addEventListener('click', showWomenProducts);
+document.getElementById('kids').addEventListener('click', showKidsProducts);
